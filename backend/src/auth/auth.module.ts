20d201f3db7f1a2fc238c45dea8fd222cb4acbd6
HaPin1 <hadrien.pinoteau@viacesi.fr>
2023-11-05 17:24:28 +0100

    Fix: JWT secret

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,13 +5,20 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      secret: 'your_secret_key',
-      signOptions: { expiresIn: '1h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_TIME'),
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService],
