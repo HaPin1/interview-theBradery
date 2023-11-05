46e09417df2fc635a4e9823134ae9339ad1311a7
HaPin1 <hadrien.pinoteau@viacesi.fr>
2023-11-05 12:50:30 +0100

    Add: User entity & authentication

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+// auth.controller.ts
+import { Controller, Post, Body, Res } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  async login(
+    @Body('username') username: string,
+    @Body('password') password: string,
+    @Res() res: Response,
+  ) {
+    const result = await this.authService.validateUser(username, password);
+
+    if (!result.sub) {
+      res.status(result.status).send(result.data);
+    }
+
+    const token = await this.authService.login(result);
+    res.status(token.status).json(token.JSON);
+  }
+
+  @Post('create')
+  async createUser(
+    @Body('username') username: string,
+    @Body('password') password: string,
+    @Res() res: Response,
+  ) {
+    const result = await this.authService.createUser(username, password);
+    res.status(result.status).send(result.data);
+  }
+}
